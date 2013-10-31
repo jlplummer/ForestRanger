82f7a340d8c6dd5dc2fa63997b98bc9e56c50bf7
wanderinweezard <wanderinweezard@gmail.com>
2013-10-31 05:34:20 -0400

    more art; starting to add code

--- ranger.rb
+++ ranger.rb
@@ -11,6 +11,11 @@ module GameConstants
 	PlayerHeight = 24
 	PlayerWidth = 21 
 
+	TileHeight = 4
+	TileWidth  = 8
+
+	SpriteFactor = 3.0
+
 	module Text
 		Caption = "Forest Ranger"
 	end
@@ -21,12 +26,53 @@ class Game < Gosu::Window
 	def initialize
 		super GameConstants::ScreenWidth, GameConstants::ScreenHeight, false
 		self.caption = GameConstants::Text::Caption
+
+		@ranger = Ranger.new(self, 0, 0)
+	end
+
+	def update
+		move_x = move_y = 0
+
+		if button_down? Gosu::KbUp then
+			move_y -= GameConstants::PlayerHeight * GameConstants::SpriteFactor
+		end
+
+		if button_down? Gosu::KbDown then
+			move_y += GameConstants::PlayerHeight * GameConstants::SpriteFactor
+		end
+
+		@ranger.move(move_x, move_y)
+	end
+
+	def draw
+		@ranger.draw
 	end
 
 	def button_down(id)
 		if id == Gosu::KbEscape
 			close
 		end
+	end
+
+end
+
+class Ranger
+
+	def initialize(window, x, y)
+		@cur_image = Gosu::Image.new(window, "media/ranger.bmp", false)
+		@x, @y = x, y
+	end
+
+	def draw
+		@cur_image.draw(@x, @y, ZOrder::Entities, GameConstants::SpriteFactor, GameConstants::SpriteFactor)
+	end
+
+	def move(x, y)
+		@x += x
+		@y += y
+	end
+
+	def shoot
 	end
 
 end
