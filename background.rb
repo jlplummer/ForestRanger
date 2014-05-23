489209065f65859d3ce828b39426e3191a567982
wanderinweezard <wanderinweezard@gmail.com>
2014-05-22 21:52:51 -0400

    done toying with backgrounds

--- background.rb
+++ background.rb
@@ -10,64 +10,50 @@ class Game < Gosu::Window
 		Gosu::enable_undocumented_retrofication
 
 		@background_tiles = Gosu::Image.load_tiles(self, "media/grass.bmp", 8, 4, false)
+		@tile_objects = Array.new
+
+		@background_tiles.each { |item| @tile_objects.push(item)}
 
 		@refresh_cooldown = 0
+		@draw_ok = true
 	end
 
 	def update
-		if @refresh_cooldown <= 0 then
-			draw_map
-		else
-			@refresh_cooldown -= 1
-		end
+		@refresh_cooldown -= 1
+		@draw_ok = true if @refresh_cooldown <= 0
 	end
 
 	def draw
-		x = 0
-		@background_tiles.each { |item| item.draw(x, 80, 0, tilefactor, tilefactor); x += (10 * tilefactor)}
-		draw_map
+		#x = 0
+		#@background_tiles.each { |item| item.draw(x, 80, 0, tilefactor, tilefactor); x += (10 * tilefactor)}
+
+		#@background_tiles[rand(@background_tiles.count)].draw(0, 150, 0, tilefactor, tilefactor)
+		
+		# I think the reason this is broke is not because I want to supress
+		# drawing, but that I want to supress generating a new set of tiles
+		if @draw_ok == true then
+			draw_map
+		end
 	end
 
 	def draw_map
 		x = y = 0
 		for j in 1..37
 			for k in 1..74
+				@background_tiles[rand(@background_tiles.count)].draw(x, y, 0, tilefactor, tilefactor)
 				#tile_to_draw = @background_tiles[rand(8)]				
-				tempTile = Tile.new(self, @background_tiles[rand(8)], x, y)
-				tempTile.draw
+				#tempTile = Tile.new(self, @background_tiles[rand(8)], x, y)
+				#tempTile.draw
 				#tile_to_draw.draw(x, y, 0, tilefactor, tilefactor)
 				y += 4
 			end
 			x += 8
 			y = 0
 		end
+
+		@draw_ok = false
+		@refresh_cooldown = 200
 	end
 end
 
-class Tile
-	attr_reader :x, :y
-
-	def initialize(window, image, x, y)
-		@cur_image = image
-		@x, @y = x, y
-	end
-
-	def draw
-		@cur_image.draw(x, y, 0, 10.0, 10.0)
-	end
-end
-
-class Map
-	def initialize(window, rows, cols)
-
-	end
-
-	def add_tile(tile)
-	end
-
-	def draw
-	end
-end
-
-window = Game.new
-window.show
+Game.new.show
