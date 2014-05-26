dd29ed36abcb0820f77a98b95e7b2ec730cf65df
wanderinweezard <wanderinweezard@gmail.com>
2014-05-26 08:02:13 -0400

    still messing with arrays

--- array.rb
+++ array.rb
@@ -6,3 +6,16 @@ tile_width = 8
 max_rows = screen_height / tile_height
 max_cols = screen_width / tile_width
 
+rows = Array.new(max_rows)
+
+for x in 0..max_rows
+	rows.push(Array.new(max_cols))
+end
+
+for x in 0..rows.count
+	for y in 0..max_cols
+		rows[x][y] = 1
+	end
+end
+
+puts rows 
